d29a15efa5d13c8477ad43bd0e7300f845b34086
sara <mohamadi.sara20@gmail.com>
2020-02-26 16:35:00 +0330

    spy detection project just to wrap up basics of js

---
+++ javascript/jsProjectLevelOne.js
@@ -0,0 +1,8 @@
+var firstName = prompt("What is your first name?")
+var lastName = prompt("What is your last name?")
+var age = prompt("How old are you?")
+var petName = prompt("What is your petname?")
+var height = prompt("How tall are you in centimeters")
+if (firstName[0] == lastName[0] && petName[petName.length - 1] == "y" && age > 20 && age < 30 && height > 170){
+  console.log(petName + "? Is that you? You little shit! Come home it's past midnight! ");
+}
