d00b1d48bcedc62228239497273c48d2c8a1054d
sara <mohamadi.sara20@gmail.com>
2020-02-26 15:19:17 +0330

    type checking added

--- javascript/testControlFlow.js
+++ javascript/testControlFlow.js
@@ -1,7 +1,10 @@
-var temp = prompt("Enter the temperature in celcius: ")
+var temp = prompt("Enter the number of temperature in celcius: ")
 
 if (!temp){
   alert("NO TEMPERATURE PROVIDED")
+}
+else if(!(typeof(temp)=="number")){
+  alert("THIS IS NOT A NUMBER! PLEASE REFRESH AND ENTER A ***NUMBER****")
 }
 
 else if (temp> 40.5){
