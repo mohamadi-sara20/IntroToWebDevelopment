932df5daee0adad8f9af5265a59e2619bf1d402b
sara <mohamadi.sara20@gmail.com>
2020-02-26 16:12:51 +0330

    tested for loops in js

---
+++ javascript/testForLoops.js
@@ -0,0 +1,14 @@
+
+for(var i = 0; i < 5;i++){
+  console.log(i)
+}
+
+var word = 'ABCDEFGHIJK'
+for(var i = 0; i < word.length; i++){
+  console.log(word[i])
+}
+
+var word = 'ABABABABABABAB'
+for(var i = 0; i < word.length; i=i+2){
+  console.log(word[i])
+}
