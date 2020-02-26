dd227c8906cf041641f5b1a74a39911e01cf91b7
sara <mohamadi.sara20@gmail.com>
2020-02-26 15:34:45 +0330

    type and null check modified"

--- javascript/testControlFlow.js
+++ javascript/testControlFlow.js
@@ -3,16 +3,19 @@ var temp = prompt("Enter the number of temperature in celcius: ")
 if (!temp){
   alert("NO TEMPERATURE PROVIDED")
 }
-else if(!(typeof(temp)=="number")){
-  alert("THIS IS NOT A NUMBER! PLEASE REFRESH AND ENTER A ***NUMBER****")
-}
-
-else if (temp> 40.5){
-  alert("HOT")
-}
-else if (temp >= 20){
-  alert("LUKEWARM")
-}
 else{
-  alert("COLD")
+  temp = parseInt(temp)
+  if(Number.isNaN(temp)){
+    alert("THIS IS NOT A NUMBER! PLEASE REFRESH AND ENTER A ***NUMBER****")
+  }
+
+  else if (temp> 40.5){
+    alert("HOT")
+  }
+  else if (temp >= 20){
+    alert("LUKEWARM")
+  }
+  else{
+    alert("COLD")
+  }
 }
