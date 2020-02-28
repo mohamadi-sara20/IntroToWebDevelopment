8ce699e9b2353565819128344ee7d4602b134725
sara <mohamadi.sara20@gmail.com>
2020-02-28 20:34:48 +0330

    game logic modified

--- javascript/gameProject/gameProject.js
+++ javascript/gameProject/gameProject.js
@@ -8,24 +8,24 @@ var six = document.getElementById('23')
 var seven = document.getElementById('31')
 var eight = document.getElementById('32')
 var nine = document.getElementById('33')
-
-
 var cells = [one, two, three, four, five, six, seven, eight, nine]
 
 function listen(c){
   c.addEventListener("click", function(){
-  counts[0] += 1;
-  if (counts[0] % 3 == 1){
-    c.textContent = 'X'
-  }
-  else if (counts[0] % 3 == 2){
-    c.textContent = 'O'
-  }
-  else{
-    c.textContent = ''
+      ind = cells.indexOf(c)
+      console.log(ind)
+      console.log(counts)
+      counts[ind] += 1;
+      if (counts[ind] % 3 == 1){
+        c.textContent = 'X'
+      }
+      else if (counts[ind] % 3 == 2){
+        c.textContent = 'O'
+      }
+      else{
+        c.textContent = ''
   }
 })
 }
-
 
 cells.forEach(listen)
