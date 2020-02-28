110beb787197209777c779db662cb35e23d66406
sara <mohamadi.sara20@gmail.com>
2020-02-28 20:18:33 +0330

    game logic modified

--- javascript/gameProject/gameProject.js
+++ javascript/gameProject/gameProject.js
@@ -1,20 +1,31 @@
-var cell = document.querySelectorAll('td')
-var clickCount = 0
+var counts = [0,0,0,0,0,0,0,0,0]
+var one = document.getElementById('11')
+var two = document.getElementById('12')
+var three = document.getElementById('13')
+var four = document.getElementById('21')
+var five = document.getElementById('22')
+var six = document.getElementById('23')
+var seven = document.getElementById('31')
+var eight = document.getElementById('32')
+var nine = document.getElementById('33')
 
 
-function countClickOnCell(c){
-  c.addEventListener('click', function(){
-    clickCount+=1;
-    if (clickCount%3 == 1){
-      c.textContent = 'X';
-    }
-    else if (clickCount%3 == 2){
-      c.textContent = 'O';
-    }
-    else {
-      c.textContent = '';
-    }
-  })
+var cells = [one, two, three, four, five, six, seven, eight, nine]
+
+function listen(c){
+  c.addEventListener("click", function(){
+  counts[0] += 1;
+  if (counts[0] % 3 == 1){
+    c.textContent = 'X'
+  }
+  else if (counts[0] % 3 == 2){
+    c.textContent = 'O'
+  }
+  else{
+    c.textContent = ''
+  }
+})
 }
 
-cell.forEach(countClickOnCell)
+
+cells.forEach(listen)
