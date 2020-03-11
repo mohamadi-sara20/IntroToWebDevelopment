394f49f963ed624da4095e232d5dd4f1210e89ea
sara <mohamadi.sara20@gmail.com>
2020-03-11 18:10:09 +0330

    views updated

--- webDjango/advancedSection/advcbv/basicApp/views.py
+++ webDjango/advancedSection/advcbv/basicApp/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView
+from django.views.generic import View, TemplateView, ListView, DetailView
 from django.http import HttpResponse
+from . import models
 
 class CBView(View):
     def get(self, request):
         return HttpResponse('CLASS-BASED VIEWS ARE COOL')
 
 class IndexView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'basicApp/index.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['injectme'] = 'BASIC INJECTION'
         return context
+
+class SchoolListView(ListView):
+    context_object_name = 'schools'
+    model = models.School
+    template_name = 'basicApp/schoolList.html'
+
+
+class SchoolDetailView(DetailView):
+    context_object_name = 'schoolsDetails'
+    model = models.School
+    template_name = 'basicApp/schoolDetail.html'
