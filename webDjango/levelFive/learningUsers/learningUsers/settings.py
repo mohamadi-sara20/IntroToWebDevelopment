65a321f23d10fab2cc2071202516942134bd7382
sara <mohamadi.sara20@gmail.com>
2020-03-11 12:55:46 +0330

    project_dir and static_root added

--- webDjango/levelFive/learningUsers/learningUsers/settings.py
+++ webDjango/levelFive/learningUsers/learningUsers/settings.py
@@ -129,8 +129,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+STATIC_URL = '/static/'
 STATICFILES_DIR = [
         STATIC_DIR,
 ]
