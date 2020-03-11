7c70798ae759aebff0ab1bf39237c349933f5276
sara <mohamadi.sara20@gmail.com>
2020-03-12 00:44:49 +0330

    updated

--- webDjango/advancedSection/advcbv/basicApp/urls.py
+++ webDjango/advancedSection/advcbv/basicApp/urls.py
@@ -4,6 +4,8 @@ from basicApp import views
 app_name = 'basicApp'
 urlpatterns = [
     re_path(r'^$', views.SchoolListView.as_view(), name='list'),
-    re_path(r'^(?P<pk>[-\w]+)/$', views.SchoolDetailView.as_view(), name='detail')
-
+    re_path(r'^(?P<pk>\d+)/$', views.SchoolDetailView.as_view(), name='detail'),
+    re_path(r'^create/$', views.SchoolCreateView.as_view(), name='create'),
+    re_path(r'^update/(?P<pk>\d+)/$',views.SchoolUpdateView.as_view(),name='update'),
+    re_path(r'^delete/(?P<pk>\d+)/$',views.SchoolDeleteView.as_view(),name='delete'),
 ]
