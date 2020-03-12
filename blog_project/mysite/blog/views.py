c6c0fce99804f9a7694eb9db0f2b8902ef7a0937
sara <mohamadi.sara20@gmail.com>
2020-03-13 00:00:58 +0330

    blog updated

--- blog_project/mysite/blog/views.py
+++ blog_project/mysite/blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from django.views.generic import (TemplateView, ListView,
                                 DetailView, CreateView,
                                 UpdateView, DeleteView)
@@ -14,8 +15,10 @@ class AboutView(TemplateView):
 class PostListView(ListView):
     model = Post
 
-    def queryset(self):
-        return Post.objects.filter(published_date__lte=timezone.now().order_by('-published_date'))
+    def get_queryset(self):
+        print('###############################')
+        print(timezone.now())
+        return Post.objects.filter(published_date__lte=timezone.now())
 
 class PostDetailView(DetailView):
     model = Post
@@ -39,3 +42,49 @@ class PostUpdateView(LoginRequiredMixin, UpdateView):
 class PostDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
     success_url = reverse_lazy('post_list')
+
+
+class DraftListView(LoginRequiredMixin, ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_list.html'
+    model = Post
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True)
+
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        print('###################')
+        print(CommentForm(request.POST))
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk = post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/comment_form.html', {'form': form})
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk)
+    comment.approve()
+    return redirect('post_detail', pk = comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post.pk)
+
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(request, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
