cc5513f5cf3dc13450a3b94b14e0bb5aced9d923
sara <mohamadi.sara20@gmail.com>
2020-03-15 00:36:55 +0330

    added search bar to customers table

--- customized_admin/my_video_rental/videos/admin.py
+++ customized_admin/my_video_rental/videos/admin.py
@@ -3,5 +3,6 @@ from videos.models import Customer, Movie
 # Register your models here.
 class CustomerAdmin(admin.ModelAdmin):
     fields = ['phone', 'first_name', 'last_name']
+    search_fields = ['phone', 'first_name', 'last_name']
 admin.site.register(Movie)
 admin.site.register(Customer, CustomerAdmin)
