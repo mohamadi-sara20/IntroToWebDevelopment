9d99d51bfa11033e8d2c346d7f64d29e67c500c7
sara <mohamadi.sara20@gmail.com>
2020-03-14 23:06:39 +0330

    extra files removed

--- simple_social_clone/simple_social/accounts/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
-from . import forms
-
-class SignUp(CreateView):
-    success_url = reverse_lazy('login')
-    template_name = 'accounts/signup.html'
-    form_class = forms.UserCreateForm
