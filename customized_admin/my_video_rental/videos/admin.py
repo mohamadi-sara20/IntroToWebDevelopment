a72e2e81e8cebf114461ea854c4386688941effd
sara <mohamadi.sara20@gmail.com>
2020-03-15 00:41:19 +0330

    added extra fields to customers table

--- customized_admin/my_video_rental/videos/admin.py
+++ customized_admin/my_video_rental/videos/admin.py
@@ -5,5 +5,6 @@ class CustomerAdmin(admin.ModelAdmin):
     fields = ['phone', 'first_name', 'last_name']
     search_fields = ['phone', 'first_name', 'last_name']
     list_filter = ['last_name', 'first_name']
+    list_display = ['last_name', 'first_name']
 admin.site.register(Movie)
 admin.site.register(Customer, CustomerAdmin)
