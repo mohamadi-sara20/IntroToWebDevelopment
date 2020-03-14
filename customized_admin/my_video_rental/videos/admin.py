6d72810d342ef8903187a3509b001ad625625296
sara <mohamadi.sara20@gmail.com>
2020-03-15 00:33:44 +0330

    order of fields changes in admin

--- customized_admin/my_video_rental/videos/admin.py
+++ customized_admin/my_video_rental/videos/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from videos.models import Customer, Movie
 # Register your models here.
-
+class CustomerAdmin(admin.ModelAdmin):
+    fields = ['phone', 'first_name', 'last_name']
 admin.site.register(Movie)
-admin.site.register(Customer)
+admin.site.register(Customer, CustomerAdmin)
